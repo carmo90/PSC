f20046c59d453148f688d0d19640c079ed2a09bd
miche <michele.trimarchi@hotmail.it>
2017-10-30 22:15:24 +0100

    cambio porta service

--- ClientApp/app/services/service.ts
+++ ClientApp/app/services/service.ts
@@ -7,11 +7,11 @@ export class StudentServices {
 
     }
     getStudentsList() {
-        return this.http.get('http://localhost:56960/api/student');
+        return this.http.get('http://localhost:65070/api/student');
     }
 
     getCourseList() {
-        return this.http.get('http://localhost:56960/api/course');
+        return this.http.get('http://localhost:65070/api/course');
     }  
 
     postData(empObj: any) {
@@ -20,7 +20,7 @@ export class StudentServices {
             'application/json; charset=utf-8'
         });
         let options = new RequestOptions({ headers: headers });
-        return this.http.post('http://localhost:56960/api/student', JSON.stringify(empObj), options);
+        return this.http.post('http://localhost:65070/api/student', JSON.stringify(empObj), options);
     }  
 
 }
