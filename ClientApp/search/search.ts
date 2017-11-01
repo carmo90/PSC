41971d54cb50402ca43876de650105897457d1d9
carmo <carmo@DESKTOP-2BVOVB0>
2017-11-01 19:25:21 +0100

    nuova richiesta

---
+++ ClientApp/search/search.ts
@@ -0,0 +1,20 @@
+﻿import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'searchFilter'
+})
+
+export class filterSearch implements PipeTransform {
+    transform(value: any, args: string): any {
+        if (args == null || args == undefined) {
+            return value;
+        }
+        else {
+            let filter = args.toLocaleLowerCase();
+            return filter ? value.filter((student: any) => (student.nome.toLocaleLowerCase().indexOf(filter) != -1)
+                || (student.cognome.toLocaleLowerCase().indexOf(filter) != -1)
+                || (student.matricola.toLocaleLowerCase().indexOf(filter) != -1)
+            ) : value;
+        }
+    }
+}
